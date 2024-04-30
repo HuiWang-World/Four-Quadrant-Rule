c3b1b5885f3f999aa806b4742cb914fc1bbc3bb8
HuiWang <HuiWang@cenboomh.com>
2024-05-01 00:46:22 +0800

    div剧中，添加颜色，字体大小

--- src/Native.tsx
+++ src/Native.tsx
@@ -4,23 +4,23 @@ function Native() {
     return (
         <>
             {
-                <div>
+                <div className='container'>
                     <div className="grid-container">
                         <div className="grid-card">
                             <label className="grid-label-urgent-important">紧急 & 重要</label>
-                            <div className="grid-grid">标签2</div>
+                            <div className="grid-grid-urgent-important">标签2</div>
                         </div>
                         <div className="grid-card">
                             <label className="grid-label-not-urgent-important">不紧急 & 重要</label>
-                            <div className="grid-grid">标签2</div>
+                            <div className="grid-grid-urgent-not-important">标签2</div>
                         </div>
                         <div className="grid-card">
                             <label className="grid-label-urgent-not-important">紧急 & 不重要</label>
-                            <div className="grid-grid">标签2</div>
+                            <div className="grid-grid-not-urgent-important">标签2</div>
                         </div>
                         <div className="grid-card">
                             <label className="grid-label-not-urgent-not-important">不紧急 & 不重要</label>
-                            <div className="grid-grid">标签2</div>
+                            <div className="grid-grid-not-urgent-not-important">标签2</div>
                         </div>
                     </div>
                 </div>
